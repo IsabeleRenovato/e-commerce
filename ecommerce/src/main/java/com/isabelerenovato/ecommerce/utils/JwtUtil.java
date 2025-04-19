83082fef925bd486ff27461b77538f80df84cbdc
Isabele <isabelerenovato@hotmail.com>
2025-04-19 16:14:00 -0300

    Creating Login API And User Details Service

--- ecommerce/src/main/java/com/isabelerenovato/ecommerce/utils/JwtUtil.java
+++ ecommerce/src/main/java/com/isabelerenovato/ecommerce/utils/JwtUtil.java
@@ -4,10 +4,13 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -37,6 +40,33 @@ public class JwtUtil {
 	private Key getSignKey() {
 		byte[] keybytes = Decoders.BASE64.decode(SECRET);
 		return Keys.hmacShaKeyFor(keybytes);
+	}
+	
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+	
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	private Claims extractAllClaims(String token) {
+		
+		return Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token).getBody();
+	}
+	
+	private Boolean isTokenExpired(String token) {
+		return extractExperation(token).before(new Date());
+	}
+
+	private Date extractExperation(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+	
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String username = extractUsername(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
 	}
 	
 	
